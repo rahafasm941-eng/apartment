ef3eafa4611c5190438efbd15ef16b8c038045a3
rahafasm941-eng <rahafasm941@gmail.com>
2026-01-07 19:02:00 +0300

    lang

---
+++ lang/ar/data.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'luxurious apartment in damascus' => 'شقة فاخرة في دمشق',
+    'luxurious' => 'فاخر',
+    'apartment' => 'شقة',
+    'Damascus' => 'دمشق',
+    'baramka' => 'البرامكة',
+    'A beautiful apartment in the heart of the city.'  => 'شقة فاخرة تقع في قلب المدينة.',
+    'area' => 'المساحة',
+    'price' => 'السعر',
+    'bathrooms' => 'غرف النوم',
+    'number_of_rooms' => 'عدد الغرف',
+    'Furnished' => 'مفروشة',
+    'yes' => 'نعم',
+    'Balcony' => 'شرفة',
+    'swimming_pool' => 'مسبح',
+    'Air Conditioning' => 'تكييف هواء',
+    'wifi' => 'واي فاي',
+    'Contact Now' => 'اتصل الآن',
+    'Reviews' => 'التقييمات',
+    'pet frindly' => 'صديق للحيوانات الأليفة',
+    'parking' => 'موقف سيارات',
+    'heating' => 'تدفئة',
+    'economic apartment' => 'شقة اقتصادية',
+    'economic' => 'اقتصادي',
+    'Homs' => 'حمص',
+    'the clock of homs' => 'ساعة حمص',
+    'A beautiful apartment with an affordable price.' => 'شقة جميلة بسعر معقول.',
+];
